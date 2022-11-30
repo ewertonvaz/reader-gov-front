3c7f37c2b6dcb6bcc2d4407e32729b0d762fefc7
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-11-30 18:36:33 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes, Outlet } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import DashBoardPage from './pages/DashBoardPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Toaster />
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        {/* NESSE MOMENTO ADICIONO UM ROUTE SEM PATH E DIGO QUE ELE IRÁ RENDERIZAR O ELEMENT E, NESTE CASO O HEADER, PARA TODOS OS ROUTES ABAIXO DELE QUE DEREM MATCH */}
-        <Route element={<><NavBar /> {/* O Header sendo renderizado */} <Outlet /> {/* Outlet será substituído pelo element dos route filho */}</>}>
-          <Route path="/dashboard" element={<DashBoardPage />} />
-          {/* <Route path="/beers" element={<ListBeersPage />} />
-          <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
-          <Route path="/random-beer" element={ <RandomBeerPage /> } />
-          <Route path="/new-beer" element={ <NewBeerPage /> } />
-          <Route path="/edit-beer/:beerId" element={ <EditBeerPage /> } /> */}
-          {/* ...*/}
-          {/* Demais Route com Header*/}
-        </Route>
-        {/*Path Coringa - sem Header*/}
         <Route path="*" element={<h1>ERROR 404</h1>}></Route>
       </Routes>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,6 @@
-import { Link } from 'react-router-dom';
-
 function HomePage() {
     return ( <div>
-        <h1>Home Pages</h1>
-        <Link to="/dashboard">Dashboard</Link>
-
-        {/* <Link to="/beers">
-            <HomeCard title="All Beers" img={beersImg} />
-        </Link>
-        <Link to="/random-beer">
-            <HomeCard title="Random Beer" img={randomBeerImg} />
-        </Link>
-        <Link to="/new-beer">
-            <HomeCard title="New Beer" img={newBeerImg} />
-        </Link> */}
+        <h1>Home Page</h1>
     </div> );
 }
 
