d4b97706eb13454a79d40a1c32b6f062251cfb13
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-12-02 11:00:10 -0300

    concluido o form de upload

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -10,18 +10,20 @@ function UploadPage() {
             const upload_file = fileInput.files[0];
             const formData = new FormData();
             formData.append('file', upload_file);
-            //const formData= { file: upload_file };
-            const request = await axios.post(
-                process.env.REACT_APP_FILE_SERVER || "http://localhost:8080/file/upload",
-                formData,
-                {
-                    headers: {
-                      'content-type': 'multipart/form-data',
-                      //'Authorization': 'json-token',
-                    },
-                }
-             );
-            console.log(request);
+            try {
+                const request = await axios.post(
+                    process.env.REACT_APP_FILE_SERVER || "http://localhost:8080/file/upload",
+                    formData,
+                    {
+                        headers: {
+                            "Content-Type": "multipart/form-data"
+                        }
+                    }
+                );
+                console.log(request);
+            } catch(e) {
+                console.log(e);
+            }
         } else {
             console.log('You need to select a file');
         }
@@ -29,12 +31,6 @@ function UploadPage() {
 
     return (
         <div>
-            {/* <h1>Upload de Arquivos</h1>
-            <form action={process.env.REACT_APP_FILE_SERVER || "http://localhost:8080/file/upload"}
-                method="post" encType="multipart/form-data">
-                    <input type="file" name="filetoupload" /><br />
-                    <input type="submit" />
-            </form> */}
             <Form onSubmit={handleSubmit} encType="multipart/form-data">
                 <Form.Group className="mb-3" controlId="formUpload">
                     <Form.Label>Upload de Arquivo</Form.Label>
