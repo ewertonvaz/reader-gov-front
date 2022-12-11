7784ad3803862dba7a5ddc808c86342f07cc2e60
Rodrigo Monclair <rmonclair@gmail.com>
2022-12-11 14:17:24 -0300

    alterações pagina de cadasto de usuário

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
 
     return (
-        <footer className="footer mt-auto py-3 bg-light">
+        <footer className="footer mt-auto py-3 bg-light" fixed="bottom">
             <div className="container">
                 <span className="text-muted">Desenvolvido por Ewerton, Priscila, Roberto e Rodrigo. <br /> Turma ENAP WDFT-92/2022</span>
             </div>
