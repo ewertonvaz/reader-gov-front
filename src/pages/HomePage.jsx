c68bd6d757db3d77c49987b0e4922bc763bae8bb
Rodrigo Monclair <rmonclair@gmail.com>
2022-12-15 01:21:53 -0300

    modal sei

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import constImg from '../assets/const-img.png'
+import SeiLoginModel from '../components/SeiLoginModel';
 
 function HomePage() {
     return (
         <div className="container mt-5">
             <h3>Página em construção...</h3>
             <img src={constImg} alt="" className='w-25 img-fluid mt-5' />
+            <SeiLoginModel />
 
         </div>
     );
